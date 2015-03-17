46d72dc893fd5eb786914132a0081e5a81353981
Lukas Stefaniak <lustefaniak@gmail.com>
2015-03-17 20:50:49 +0100

    Setup State on init

--- src/main/scala/thegame/TheGame.scala
+++ src/main/scala/thegame/TheGame.scala
@@ -4,8 +4,16 @@ import org.scalajs.dom.CanvasRenderingContext2D
 
 trait TheGame {
 
-  def init(windowSize: Vect): Unit = {
+  class State(windowSize: Vect) {
+    val unitSize = windowSize.y / 40
+    var paddleHeight = unitSize * 7
+    var paddleMiddle = windowSize.y / 2
+  }
 
+  var state: State = _
+
+  def init(windowSize: Vect): Unit = {
+    state = new State(windowSize)
   }
 
   def update(): Unit = {
